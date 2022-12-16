8006e4afb2bc25841e48463d20617935cecf670e
ganesh <ganeshsadhashivam@outlook.com>
2022-12-16 19:13:53 +0530

    cart

---
+++ src/data.js
@@ -0,0 +1,33 @@
+export default [
+  {
+    id: 1,
+    title: "Apple 16 Mac Book Pro  ",
+    price: 350000,
+    image:
+      "https://petapixel.com/assets/uploads/2021/11/Macbook-Pro-M1-Max-Review-for-Photographers-1-copy-800x534.jpg",
+    amount: 1,
+  },
+  {
+    id: 2,
+    title: "Samsung S22 ultra",
+    price: 130000,
+    image: "https://m.media-amazon.com/images/I/71PvHfU+pwL._SL1500_.jpg",
+    amount: 1,
+  },
+  {
+    id: 3,
+    title: "System Design by Alex xu",
+    price: 3000.5,
+    image:
+      "https://images-na.ssl-images-amazon.com/images/I/51XdrsXkWML._AC_UL900_SR615,900_.jpg",
+    amount: 1,
+  },
+  {
+    id: 4,
+    title: "The Algorithm design Manual by steven",
+    price: 3000.05,
+    image:
+      "https://i.pinimg.com/originals/d8/3f/d2/d83fd249e92015b60a6cae00666c60cb.jpg",
+    amount: 1,
+  },
+];
